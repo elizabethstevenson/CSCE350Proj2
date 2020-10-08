f2ac3f251ce8cbad1f5b26c68f45393f049408a5
elizabethstevenson <60235792+elizabethstevenson@users.noreply.github.com>
2020-10-08 07:54:04 -0700

    Update proj2.cpp

--- proj2.cpp
+++ proj2.cpp
@@ -15,6 +15,23 @@ using std::vector;
 vector<string> people;
 vector<int> ability;
 
+class Team {
+    public:
+        string teamName;
+        
+        Team(string teamName) {
+            this.teamName = teamName;
+        }
+        string getName() {
+            return teamName;
+        }
+        void setName(string teamName) {
+            this.teamName = teamName;
+        }
+}
+vector<Team> teams;
+
+
 //string vector printer
 void printStringVector(vector<string> s) {
     for (unsigned int i = 0; i < s.size(); i++) {
@@ -44,9 +61,38 @@ void createVectors(int n) {
 
     ability.erase(ability.begin(), ability.begin() + n);//had to remove the first n elements bc they were all 0's
     
-    printStringVector(people);
-    printIntVector(ability);
+    //printStringVector(people);
+    //printIntVector(ability);
+}
 
+public teamGenerator() {
+    teams.push_back(new Team("Team A"));
+    teams.push_back(new Team("Team B"));
+    int n = 0;
+    while(people.size() > 0) {
+        int highestPersonIndex = findHighestAbility(ability);
+        teams.get(n).people.add(people.get(highestPersonIndex));
+        people.remove(highestPersonIndex);
+        ability.remove(highestPersonIndex);
+
+        if(n < teams.size()-1) {
+            n++;
+        } else {
+            n = 0;
+        }
+    }
+}
+
+public int findHighestAbility(vector<int> s) {
+    int index = -1;
+    int highest = -1;
+    for (int i = 0; i < s.size(); ++i) {
+        if(s.get(i) > highest) {
+            highest = s.get(i)
+            index = i;
+        }
+    }
+    return index;
 }
 
 int main(int argc, char* argv[]) {
